dd3d6533d5d700ee99b56970d9fd3031e40391bc
Jose Maria Almonte <80287073+jopealmonte@users.noreply.github.com>
2023-08-21 14:36:30 -0300

    Delete Tablas directory

--- Tablas/tabla_modelo.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE tabla_prueba (Idnum VARCHAR(20), productÑservice VARCHAR(20), Name VARCHAR(20))
-
-CREATE TRIGGER elimination_trigger_afterdelete AFTER DELETE ON tabla_prueba FOR EACH ROW 
-INSERT INTO 
