1617c1a80481b4d18a9cb4c68b896377d022ac40
Jose Maria Almonte <80287073+jopealmonte@users.noreply.github.com>
2023-08-20 21:02:33 -0300

    Update tabla_modelo.sql

--- Tablas/tabla_modelo.sql
+++ Tablas/tabla_modelo.sql
@@ -1,1 +1,4 @@
 CREATE TABLE tabla_prueba (Idnum VARCHAR(20), Product/Service VARCHAR(20), Name VARCHAR(20))
+
+CREATE TRIGGER elimination_trigger_afterdelete AFTER DELETE ON tabla_prueba FOR EACH ROW 
+INSERT INTO 
